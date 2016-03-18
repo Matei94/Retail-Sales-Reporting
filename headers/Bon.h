744aeebdd516efa623b587dbd47c7bc217c30de9
steaburdea13robert <steaburdea_robert@yahoo.com>
2016-03-18 22:38:08 +0200

    Added Bon.h and Bon.cpp

---
+++ headers/Bon.h
@@ -0,0 +1,38 @@
+#ifndef __bon_h__
+#define __bon_h__
+
+#include <string.h>
+
+class Bon{
+
+public:
+
+	Bon( std::string idBon, std::int idProdus );
+
+	Bon( const Bon& b );
+
+	void operator= ( const Bon& b );
+
+	~Bon();
+
+	void setIdBon( std::string idBon );
+
+	void setIdProdus( std::int idProdus );
+
+	string getIdBon( std::string idBon );
+
+	int getIdProdus( std::int idProdus );
+
+friend:
+	
+	std::ostream& operator<< ( std::ostream& out, const Bon& b );
+	std::istream& operator<< ( std::istream& in, Bon& b );
+
+private:
+
+	std::string idBon;
+	std::int idProdus;
+
+};
+
+#endif __bon_h__
