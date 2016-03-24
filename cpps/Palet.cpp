f844da3729431885a4149f804b947f9a0e6cdf6a
Roxana Nicolescu <nicolescu_roxana@yahoo.ro>
2016-03-24 14:35:22 +0200

    am adaugat clasa Palet

---
+++ cpps/Palet.cpp
@@ -0,0 +1,37 @@
+#include "../headers/Palet.h"
+
+string Palet::get_id_palet () {
+	return id_palet;
+}
+
+int Palet::get_n_items () {
+	return n_items;
+}
+
+int Palet::get_slot () {
+	return get_slot;
+}
+
+int Palet::get_prod_type () {
+	return prod_type;
+}
+/*Palet::~Palet () {
+};
+
+Palet::Palet (char *id_palet, int prod_type, int n_items, int slot) {
+	strcpy (this->id_palet, id_palet);
+	this->prod_type = prod_type;
+	this->n_items = n_items;
+	this->slot = slot;
+}
+Palet::Palet (const Palet & palet) {
+	strcpy (this->id_palet, palet.id_palet);
+	this->prod_type = palet.id_palet;
+	this->n_items = palet.n_items;
+	this->slot = palet.slot;
+}*/
+
+
+
+
+	
---
+++ headers/Palet.h
@@ -0,0 +1,18 @@
+#ifndef __paleti_H_
+#define __paleti_H_
+#include<string.h>
+using namespace std;
+
+class Palet {
+	string id_palet;
+	int prod_type, n_items, slot;
+	public:
+		string get_id_palet ();
+		int get_prod_type ();
+		int get_n_items ();
+		int get_slot ();
+	//	~Palet (); //destructor
+	//	Palet (string id_palet, int prod_type, int n_items, int slot);    //constructor
+	//	Palet (const Palet& palet); //copy-constructor
+};
+#endif
