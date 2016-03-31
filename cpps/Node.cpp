07f5ac9b6ae51f0fce88d5c249a2ceff398a226f
Rizescu Eusebiu <rizescueusebiu@yahoo.com>
2016-03-31 19:20:22 +0300

    Node.h si Node.cpp de care o sa avem nevoie la lista, coada, hashtable, baza bazelor

---
+++ cpps/Node.cpp
@@ -0,0 +1,66 @@
+#include <cstddef>
+
+#include "Node.h"
+
+template <typename T>
+Node<T>::Node() {
+  this->value = 0;
+  this->next = NULL;
+  /*this->prev = NULL;*/
+}
+
+template <typename T>
+Node<T>::Node(T value) {
+  Node<T>();
+  this->value = value;
+}
+
+template <typename T>
+Node<T>::Node(const Node<T>& n) {
+  this->value = n.value;
+  this->next  = n.next;
+  /*this->prev  = n.prev;*/
+}
+
+template <typename T>
+void Node<T>::operator=(const Node<T>& n) {
+  this->value = n.value;
+  this->next  = n.next;
+  /*this->prev  = n.prev;*/
+}
+
+template <typename T>
+Node<T>::~Node() {
+  /* Do nothing */
+}
+
+template <typename T>
+T Node<T>::getValue() {
+  return this->value;
+}
+
+template <typename T>
+Node<T> *Node<T>::getNext() {
+  return this->next;
+}
+
+/*template <typename T>
+Node<T> *Node<T>::getPrev() {
+  return this->prev;
+}*/
+
+template <typename T>
+void Node<T>::setNext(Node<T>* n) {
+  this->next = n;
+}
+
+/*template <typename T>
+void Node<T>::setPrev(Node<T>* n) {
+  this->prev = prev;
+}*/
+
+
+//!!!!! AICI TREBUIE ADAUGAT CE O SA FOLOSIM, CE TIPURI DE DATE
+
+template class Node<int>;
+template class Node<float>;
