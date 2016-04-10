5d9dcff1dc5aaf49d05332203687f0d233da8ff6
Paula-Elena <margarit.paula@gmail.com>
2016-04-10 14:42:43 +0300

    Uite, Codi

--- sd/Tranzactie.cpp
+++ sd/Tranzactie.cpp
@@ -53,14 +53,31 @@ string Tranzactie::getIdBon( ) {
 }
 
 Tranzactie::Tranzactie( string linieFisier ){
+	/* firstComma = pozitia primei virgule din stringul dat ca parametru */
 	int firstComma = linieFisier.find_first_of( ',' );
+	
+	/* Se extrage substringul din intervalul [0, firstComma] = idTranzactie*/
 	this->idTranzactie = stoi( linieFisier.substr( 0, firstComma ) );
+	
+	/* Se elimina substringul anterior si se va lucra pe stringul nou = sirulDeProcesat */
 	string sirulDeProcesat = linieFisier.substr( firstComma + 1, linieFisier.length( ) - firstComma - 1 );
+
+	/* Se cauta urmatoarea aparitie a unei virgule */
 	firstComma = sirulDeProcesat.find_first_of( ',' );
+
+	/* Se extrage timeStamp-ul din noul sir */
 	this->timeStamp = sirulDeProcesat.substr( 0, firstComma );
+
+	/* Se updateaza sirul de procesat, eliminand substringul extras anterior */
 	sirulDeProcesat = sirulDeProcesat.substr( firstComma + 1, sirulDeProcesat.length( ) - firstComma - 1 );
+
+	/* Se cauta aparitia urmatoarei virgule */
 	firstComma = sirulDeProcesat.find_first_of( ',' );
+
+	/* Se extrage idBon din noul sir */
 	this->idBon = sirulDeProcesat.substr( 0, firstComma );
+
+	/* Se extrage idMagazin din ceea ce ramane din string */
 	this->idMagazin = stoi( sirulDeProcesat.substr( firstComma + 1, sirulDeProcesat.length( ) - firstComma - 1 ) );
 }
 
